e34be486ae84fbf2ccc73fdcf0a8ebcd928d1d65
Peter Etherington <pjg.etherington@gmail.com>
2020-02-06 17:10:13 -0500

    Setup pug, nodemon, dotenv

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 'use strict';
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
+app.set('view engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route('/').get((req, res) => {
+	res.render(process.cwd() + '/views/pug/index.pug');
+});
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+app.listen(process.env.PORT, () => {
+	console.log('Listening on port ' + process.env.PORT);
 });
