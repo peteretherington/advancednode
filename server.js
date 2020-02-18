29b0bce026684ac0c8e45c97dfe9bd1f8dd5879e
Peter Etherington <pjg.etherington@gmail.com>
2020-02-17 22:36:27 -0500

    Set up passport

--- server.js
+++ server.js
@@ -5,20 +5,34 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
 
-app.set('view engine', 'pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'pug');
+
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: true,
+		saveUninitialized: true
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route('/').get((req, res) => {
 	res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' });
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log('Listening on port ' + process.env.PORT);
 });
