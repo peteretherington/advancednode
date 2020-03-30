cc9fafbb1d6e5273a6cde3eccbaa43a27b40e399
Peter Etherington <pjg.etherington@gmail.com>
2020-03-29 20:48:10 -0400

    only require dotenv in development

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const bodyParser = require('body-parser');
 const express = require('express');
