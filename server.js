b8984b92369929516599652a112bc903339134af
Peter Etherington <pjg.etherington@gmail.com>
2020-05-05 00:02:35 -0400

    Register user

--- server.js
+++ server.js
@@ -67,8 +67,43 @@ mongo.connect(process.env.DATABASE, { useUnifiedTopology: true }, (err, client) 
 		}
 
 		app.route('/').get((req, res) => {
-			res.render(process.cwd() + '/views/pug/index', { title: 'Home page', message: 'login', showLogin: true });
+			res.render(process.cwd() + '/views/pug/index', {
+				title: 'Home page',
+				message: 'login',
+				showLogin: true,
+				showRegistration: true,
+			});
 		});
+
+		app.route('/register').post(
+			(req, res, next) => {
+				db.collection('users').findOne({ username: req.body.username }, (err, user) => {
+					if (err) {
+						next(err);
+					} else if (user) {
+						res.redirect('/');
+					} else {
+						db.collection('users').insertOne(
+							{
+								username: req.body.username,
+								password: req.body.password,
+							},
+							(err, doc) => {
+								if (err) {
+									res.redirect('/');
+								} else {
+									next(null, user);
+								}
+							}
+						);
+					}
+				});
+			},
+			passport.authenticate('local', { failureRedirect: '/' }),
+			(req, res) => {
+				res.redirect('/profile');
+			}
+		);
 
 		app.route('/login').post(passport.authenticate('local', { successRedirect: '/profile', failureRedirect: '/' }));
 
@@ -79,6 +114,10 @@ mongo.connect(process.env.DATABASE, { useUnifiedTopology: true }, (err, client) 
 
 		app.route('/profile').get(ensureAuthenticated, function (req, res) {
 			res.render(process.cwd() + '/views/pug/profile', { username: req.user.username });
+		});
+
+		app.use((req, res) => {
+			res.status(404).type('text').send('Not Found');
 		});
 
 		app.listen(process.env.PORT || 3000, () => {
